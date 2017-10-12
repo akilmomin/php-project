6474fc8350b4178ae3252fb161ab46027d1f3b5b
Akil Momin <akilmomin92@gmail.com>
2017-10-11 21:11:01 -0500

    cart.php

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
         <li><a href="contact.php">CONTACT</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart">Cart</span></a></li>
+        <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart">Cart</span></a></li>
         <li data-toggle="modal" data-target="#myModal"><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
             <div class="modal fade" id="myModal" role="dialog">
             <div class="modal-dialog">
