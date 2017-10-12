2de81a047fecebdf8dbe665b5d332767230de379
Akil Momin <akilmomin92@gmail.com>
2017-10-12 11:20:16 -0500

    my account

--- myaccount.php
+++ myaccount.php
@@ -41,6 +41,13 @@
           <br>
           <input class="btn" type="submit" name="Submit" value="Submit">
     </form>
+    <hr>
+    <form>
+        <center>
+          <label>Delete Account</label><br>
+          <input class="btn btn-danger" type="submit" name="Submit" value="Delete">
+        </center>
+    </form>
     </div>
     <div class="col-sm-4">
       
